1afdedef2aceb37e6c959315f119fd4a5af811d9
adigiro <anthony.d@asu.edu>
2009-11-05 19:11:31 -0700

    Added license info

--- mkinit.c
+++ mkinit.c
@@ -1,6 +1,24 @@
 // Anthony DiGirolamo
 // Random Initial Condition Generator
 
+/* This file is part of an MPI training course.
+ *
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009 Anthony DiGirolamo
+ */
+
 #define INITIAL "initial_condition"
 #define LIVE '1'
 #define DEAD '0'
